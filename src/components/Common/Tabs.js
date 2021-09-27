e22410b1a3e637c0aec43b65546b17171ae23c92
Jared <kuil5230@gmail.com>
2021-09-27 23:21:17 +0800

    feat: Add tabs component

---
+++ src/components/Common/Tabs.js
@@ -0,0 +1,59 @@
+import { useState, useRef } from 'react';
+
+import Link from '@/components/Common/Link';
+
+const Tabs = (props) => {
+  const { data = [] } = props;
+
+  const [tabBoundingBox, setTabBoundingBox] = useState(null);
+  const [wrapperBoundingBox, setWrapperBoundingBox] = useState(null);
+  const [highlightedTab, setHighlightedTab] = useState(null);
+  const [isHoveredFromNull, setIsHoveredFromNull] = useState(true);
+
+  const highlightRef = useRef(null);
+  const wrapperRef = useRef(null);
+
+  const repositionHighlight = (e, tab) => {
+    setTabBoundingBox(e.target.getBoundingClientRect());
+    setWrapperBoundingBox(wrapperRef.current.getBoundingClientRect());
+    setIsHoveredFromNull(!highlightedTab);
+    setHighlightedTab(tab);
+  };
+
+  const resetHighlight = () => setHighlightedTab(null);
+
+  const highlightStyles = {};
+  if (tabBoundingBox && wrapperBoundingBox) {
+    highlightStyles.transitionDuration = isHoveredFromNull ? '0ms' : '150ms';
+    highlightStyles.opacity = highlightedTab ? 1 : 0;
+    highlightStyles.width = `${tabBoundingBox.width}px`;
+    highlightStyles.transform = `translate(${tabBoundingBox.left - wrapperBoundingBox.left}px)`;
+  }
+
+  return (
+    <ul className="relative" ref={wrapperRef} onMouseLeave={resetHighlight}>
+      <li
+        className="absolute left-0 py-4 duration-150 bg-gray-300 rounded-md dark:bg-gray-600 top-1"
+        ref={highlightRef}
+        style={{
+          ...highlightStyles,
+          transitionProperty: 'width, transform, opacity',
+        }}
+      />
+      {data.map((tab) => (
+        <li key={tab.title} className="relative inline-block">
+          <Link
+            className="inline-block p-2 text-gray-700 transition duration-100 cursor-pointer dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-300"
+            href={tab.pathname}
+            aria-label={tab.title}
+            onMouseOver={(ev) => repositionHighlight(ev, tab)}
+          >
+            {tab.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Tabs;
--- src/components/Layout/Navigation/DesktopNav.js
+++ src/components/Layout/Navigation/DesktopNav.js
@@ -1,28 +1,20 @@
 import { memo } from 'react';
 
-import Link from '@/components/Common/Link';
 import ThemeToggle from '@/components/Layout/ThemeToggle';
+
+import Tabs from '@/components/Common/Tabs';
 
 const DesktopNav = (props) => {
   const { routes = [] } = props;
 
   return (
-    <ul className="items-center hidden h-10 sm:flex">
-      {routes.map((item, index) => (
-        <li className="flex justify-center mx-1" key={`${index}`}>
-          <Link
-            className={`px-2 py-1 transition duration-200 ease-in-out rounded-md cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700 hover:text-gray-700 dark:hover:text-gray-300`}
-            href={item.pathname}
-            aria-label={item.title}
-          >
-            {item.title}
-          </Link>
-        </li>
-      ))}
-      <li className="flex flex-col justify-center mx-2">
+    <div className="items-center hidden h-10  sm:flex">
+      <Tabs data={routes} />
+
+      <div className="flex flex-col justify-center mx-2">
         <ThemeToggle />
-      </li>
-    </ul>
+      </div>
+    </div>
   );
 };
 
