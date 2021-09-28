f5eb78b9ffbc5890503aeb6014765f6ea05a8fb0
Jared <jared@tagtoo.com>
2021-09-28 17:31:43 +0800

    chore(code-block): update style

--- src/components/Common/CodeBlock.js
+++ src/components/Common/CodeBlock.js
@@ -11,7 +11,7 @@ const CodeBlock = ({ children, className }) => {
             <div className="self-end px-3 py-1 mx-5 text-gray-200 bg-gray-700 rounded-t-lg ">
               {language.toUpperCase()}
             </div>
-            <pre className="w-screen p-5 overflow-scroll bg-gray-700 rounded-md md:w-full">
+            <pre className="w-screen p-5 overflow-scroll bg-gray-700 rounded-md md:max-w-2xl lg:max-w-3xl md:w-full">
               {tokens.map((line, i) => (
                 <div key={i} {...getLineProps({ line, key: i })}>
                   {line.map((token, key) => (
