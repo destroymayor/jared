6d753e44982eebc954100c0934d6357a6c72f1e9
Jared <jared@tagtoo.com>
2021-09-29 12:25:56 +0800

    chore(code-block): Add theme in component props

--- src/components/Common/CodeBlock.js
+++ src/components/Common/CodeBlock.js
@@ -1,31 +1,30 @@
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import vsDark from 'prism-react-renderer/themes/vsDark';
 
 const CodeBlock = ({ children, className }) => {
   const language = className.replace(/language-/, '');
 
   return (
-    <Highlight {...defaultProps} code={children} language={language}>
-      {({ tokens, getLineProps, getTokenProps }) => {
-        return (
-          <div className="flex flex-col items-start">
-            <div className="self-end px-3 py-1 mx-5 text-gray-200 bg-gray-700 rounded-t-lg ">
-              {language.toUpperCase()}
-            </div>
-            <pre
-              className="p-5 overflow-scroll bg-gray-700 rounded-md md:max-w-2xl lg:max-w-3xl md:w-full"
-              style={{ width: '95vw' }}
-            >
-              {tokens.map((line, i) => (
-                <div key={i} {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
-            </pre>
+    <Highlight {...defaultProps} theme={vsDark} code={children} language={language}>
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <div className="flex flex-col items-start">
+          <div className="self-end px-3 py-1 mx-5 text-gray-200 bg-gray-700 rounded-t-lg ">
+            {language.toUpperCase()}
           </div>
-        );
-      }}
+          <pre
+            className="p-5 overflow-scroll bg-gray-700 rounded-md md:max-w-2xl lg:max-w-3xl md:w-full"
+            style={{ width: '95vw' }}
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        </div>
+      )}
     </Highlight>
   );
 };
