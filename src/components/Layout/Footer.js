e579de2976429ea3e53a2e9728b36e5767ad7092
Jared <kuil5230@gmail.com>
2021-10-03 14:19:16 +0800

    chore(layout/footer): Update style

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="flex justify-center flex-shrink-0 px-4 py-8 md:max-w-2xl">
-    <div className="text-xs text-gray-500 dark:text-gray-400">
+    <div className="text-xs text-gray-600 dark:text-gray-300">
       © {new Date().getFullYear()} Jared Chen. All Rights Reserved.
     </div>
   </footer>
