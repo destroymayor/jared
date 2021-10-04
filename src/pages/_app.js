1d68ce3b450ce17a58d3faf53715cf32aacfabc3
Jared <jared@tagtoo.com>
2021-10-04 16:27:04 +0800

    feat(common): Add GA4 script component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 
 import { ThemeProvider } from 'next-themes';
 import Layout from '@/components/Layout/Layout';
+import GA4 from '@/components/Common/GA4';
 
 const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider disableTransitionOnChange attribute="class">
     <Layout>
       <Component {...pageProps} />
+
+      <GA4 />
     </Layout>
   </ThemeProvider>
 );
