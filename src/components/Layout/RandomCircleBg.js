2a4d63a3a520051e275fb88fe34a9f4a6245d137
Jared <jared@tagtoo.com>
2021-10-04 15:17:28 +0800

    chore(layout/random-circle-bg): Update style

--- src/components/Layout/RandomCircleBg.js
+++ src/components/Layout/RandomCircleBg.js
@@ -1,21 +1,10 @@
 import CircleSpring from '@/components/Common/CircleSpring';
 
-const colors = [
-  'bg-gray-700',
-  'bg-red-700',
-  'bg-yellow-700',
-  'bg-green-700',
-  'bg-blue-700',
-  'bg-indigo-700',
-  'bg-purple-700',
-  'bg-pink-700',
-];
-
 const RandomCircleBg = () => {
   const circleList = [
-    { className: 'w-36 h-36', styles: { top: -35, right: -40 }, delay: 350 },
-    { className: 'w-44 h-44', styles: { top: 650, left: -100 }, delay: 350 },
-    { className: 'w-44 h-44', styles: { bottom: 0, right: -100 }, delay: 350 },
+    { className: 'w-36 h-36 bg-blue-500', styles: { top: -35, right: -40 }, delay: 350 },
+    { className: 'w-44 h-44 bg-blue-600', styles: { top: 650, left: -100 }, delay: 350 },
+    { className: 'w-44 h-44 bg-blue-700', styles: { bottom: 0, right: -100 }, delay: 350 },
   ];
 
   return (
@@ -24,9 +13,7 @@ const RandomCircleBg = () => {
         return (
           <CircleSpring
             key={`${index}`}
-            className={`absolute rounded-full ${
-              colors[Math.floor(Math.random() * colors.length)]
-            } ${circle.className}`}
+            className={`absolute rounded-full ${circle.className}`}
             style={{ ...circle.styles, zIndex: -1 }}
             delay={circle.delay}
           />
