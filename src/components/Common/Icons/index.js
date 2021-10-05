6ab43bed2fc1486288f8238d1c86cd9384ddf3be
Jared <jared@tagtoo.com>
2021-10-05 10:54:18 +0800

    chore: Rename components

--- src/components/Common/Icons/index.js
+++ src/components/Common/Icons/index.js
@@ -1,2 +1,2 @@
-export { default as GithubIcon } from '@/components/Common/Icons/github';
-export { default as LinkedInIcon } from '@/components/Common/Icons/linkedIn';
+export { default as GithubIcon } from '@/components/Common/Icons/GithubIcon';
+export { default as LinkedInIcon } from '@/components/Common/Icons/LinkedInIcon';
