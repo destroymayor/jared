cab567215df0e2864d86df0dc6f24a813a7cc3dc
Jared <kuil5230@gmail.com>
2021-10-11 19:02:32 +0800

    chore(TopTracks): Add album in tracks

--- src/components/Music/TopTracks.js
+++ src/components/Music/TopTracks.js
@@ -1,6 +1,8 @@
 import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 
+import Image from 'next/image';
+
 import Link from '@/components/Common/Link';
 
 const TopTracks = () => {
@@ -9,16 +11,27 @@ const TopTracks = () => {
   if (!data) return null;
 
   return (
-    <div className="flex flex-col mt-12 gap-y-2 ">
+    <div className="flex flex-col mt-12 gap-y-2">
       <h2 className="text-2xl">Top Tracks</h2>
       <p className="dark:text-gray-400">{`Here's my top tracks on Spotify updated daily.`}</p>
-      <ul className="flex flex-col gap-y-6">
+
+      <ul className="flex flex-col gap-y-2">
         {data?.tracks?.map((item, index) => (
           <li
-            key={`${item.title} - ${item.artist}`}
-            className="flex border-b-[1px] border-gray-400 dark:border-gray-600 py-2 gap-x-2"
+            key={`${item.title} - ${item.artist} - ${index}`}
+            className="flex items-center border-b-[1px] border-gray-400 dark:border-gray-600 py-2 gap-x-4"
           >
-            <div className="dark:text-gray-400">{index + 1}</div>
+            <div className="w-5 text-center dark:text-gray-400">{index + 1}</div>
+
+            <div className="bg-gray-200 w-[64px] h-[64px]">
+              <Image
+                unoptimized
+                alt={item?.album?.title}
+                src={item?.album?.url}
+                width={64}
+                height={64}
+              />
+            </div>
             <div>
               {item?.songUrl ? (
                 <Link className="font-medium truncate" href={item?.songUrl}>
@@ -27,8 +40,7 @@ const TopTracks = () => {
               ) : (
                 <p className="font-medium">Not Playing</p>
               )}
-              <span className="mx-2 ">{'–'}</span>
-              <span className="text-gray-500 dark:text-gray-400">{item?.artist ?? 'Spotify'}</span>
+              <p className="text-gray-500 dark:text-gray-400">{item?.artist ?? 'Spotify'}</p>
             </div>
           </li>
         ))}
--- src/pages/api/top-tracks.js
+++ src/pages/api/top-tracks.js
@@ -6,12 +6,14 @@ export default async function handler(req, res) {
   if (response.status === 204 || response.status > 400) {
     return res.status(200).json({ tracks: [] });
   }
+
   const { items } = await response.json();
 
-  const tracks = items.slice(0, 10).map((item) => ({
-    artist: item?.track?.artists?.map((_artist) => _artist.name).join(', '),
-    songUrl: item?.track?.external_urls?.spotify,
-    title: item?.track?.name,
+  const tracks = items.slice(0, 10).map(({ track }) => ({
+    album: track?.album?.images.filter((image) => image.width === 64)?.[0],
+    artist: track?.artists?.map((_artist) => _artist.name).join(', '),
+    songUrl: track?.external_urls?.spotify,
+    title: track?.name,
   }));
 
   res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=43200');
