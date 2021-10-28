7ec4ed4ba31d22c6de600b37f4185245daf901f8
Jared <jared@tagtoo.com>
2021-10-28 14:57:58 +0800

    chore(pages/_document): Use functional component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,27 +1,22 @@
-// eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="preload"
-            href="/fonts/source-code-pro.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="anonymous"
-          />
-        </Head>
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/source-code-pro.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </Head>
 
-        <body className="transition duration-200 ease-in-out bg-gray-50 dark:bg-black">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+      <body className="transition duration-200 ease-in-out bg-gray-50 dark:bg-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
