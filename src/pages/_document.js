414bdf557a3d60b95f0634aac00861b3ef7e5da3
Jared <jared@tagtoo.com>
2021-11-19 12:45:01 +0800

    fix dark mode FOUC

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/source-code-pro.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-      </Head>
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/source-code-pro.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+        </Head>
 
-      <body className="bg-gray-50 dark:bg-black">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+        <body className="bg-gray-50 dark:bg-black">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
