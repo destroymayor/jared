ee00f0e4aec613f03a810d0910e2395f6dd1358b
Jared <jared@tagtoo.com>
2022-03-22 15:43:43 +0800

    chore: Update style

--- src/components/Common/Tag.js
+++ src/components/Common/Tag.js
@@ -7,14 +7,14 @@ const Tag = (props) => {
     default: 'border-zinc-200 bg-zinc-500 text-zinc-600',
     info: 'border-blue-300 bg-blue-500 text-blue-600',
     warning: 'border-orange-200 bg-orange-500 text-orange-600',
-    success: 'border-green-200 bg-green-500 text-green-600',
+    success: 'border-green-400 bg-green-500 text-green-600',
     error: 'border-red-200 bg-red-500 text-red-600',
   };
 
   return (
     <span
       className={clsx(
-        'flex flex-none items-center justify-center gap-2 whitespace-nowrap rounded-full border bg-opacity-5  px-3 py-[2px] text-xs font-semibold uppercase tracking-wide hover:bg-opacity-10 dark:bg-opacity-10 dark:hover:bg-opacity-30',
+        'flex flex-none cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-full border bg-opacity-5 px-3 py-[2px] text-xs font-semibold uppercase tracking-wide hover:bg-opacity-40 dark:bg-opacity-10 dark:hover:bg-opacity-30',
         `${types?.[type]} ${className}`
       )}
     >
--- src/pages/tools.js
+++ src/pages/tools.js
@@ -16,25 +16,30 @@ export default function Tools() {
       <h1 className="text-2xl sm:text-4xl">{title}</h1>
       <p className="text-md py-4 dark:text-gray-400 sm:text-lg">{description}</p>
 
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
         {tools.map((tool, index) => {
           const animationDelay = `${index * 20}ms`;
 
           return (
-            <Link href={tool.link} key={tool.title}>
-              <FadeInSection className="flex items-center gap-4" delay={animationDelay}>
-                <span className="grid max-h-[70px] min-h-[70px] min-w-[70px] max-w-[70px] items-center rounded-md bg-zinc-200 p-2 dark:bg-zinc-800">
-                  {tool.logo}
-                </span>
-                <div className="flex flex-col gap-1">
-                  <div className="flex items-center gap-2">
-                    <h2 className="text-xl">{tool.title}</h2>
-                    {tool.tag && <Tag label={tool.tag.label} type={tool.tag.type} />}
-                  </div>
-                  <p className="text-sm dark:text-zinc-400">{tool.description}</p>
+            <FadeInSection
+              key={tool.title}
+              className="flex items-center gap-4 rounded-md p-2 hover:bg-zinc-200 dark:hover:bg-zinc-900"
+              delay={animationDelay}
+            >
+              <span className="grid max-h-[70px] min-h-[70px] min-w-[70px] max-w-[70px] items-center rounded-md p-2 dark:bg-zinc-800">
+                {tool.logo}
+              </span>
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-2">
+                  <Link href={tool.link}>
+                    <h2 className="text-xl hover:underline">{tool.title}</h2>
+                  </Link>
+
+                  {tool.tag && <Tag label={tool.tag.label} type={tool.tag.type} />}
                 </div>
-              </FadeInSection>
-            </Link>
+                <p className="text-sm dark:text-zinc-400">{tool.description}</p>
+              </div>
+            </FadeInSection>
           );
         })}
       </div>
