cde3ed63d1ddc3e5ac62bd5e2ab5d84dcc537ad0
Jared <kuil5230@gmail.com>
2022-04-16 19:45:13 +0800

    fix: Update style

--- src/components/CodingActive/Overview.js
+++ src/components/CodingActive/Overview.js
@@ -1,3 +1,10 @@
+const renderItem = (label, text) => (
+  <div className="col-span-2 flex flex-col justify-around rounded-xl bg-zinc-100 py-2 px-4 shadow-md dark:bg-zinc-900 sm:col-span-1">
+    <span className="text-sm dark:text-zinc-400">{label}</span>
+    <span className="text-lg font-semibold">{text}</span>
+  </div>
+);
+
 export default function Overview(props) {
   const { data } = props;
 
@@ -8,25 +15,10 @@ export default function Overview(props) {
 
   return (
     <div className="grid grid-cols-2 gap-2 py-2">
-      <div className="col-span-2 flex flex-col justify-around rounded-xl bg-zinc-100 py-2 px-4 shadow-md dark:bg-zinc-900 sm:col-span-1">
-        <span className="text-sm dark:text-zinc-400">Best Day</span>
-        <span className="text-lg font-semibold">{bestDay}</span>
-      </div>
-
-      <div className="col-span-2 flex flex-col justify-around rounded-xl bg-zinc-100 py-2 px-4 shadow-md dark:bg-zinc-900 sm:col-span-1">
-        <span className="text-sm dark:text-zinc-400">Daily Average</span>
-        <span className="text-lg font-semibold">{dailyAverage}</span>
-      </div>
-
-      <div className="col-span-2 flex flex-col justify-around rounded-xl bg-zinc-100 py-2 px-4 shadow-md dark:bg-zinc-900 sm:col-span-1">
-        <span className="text-sm dark:text-zinc-400">Last Week</span>
-        <span className="h-5 w-1/2 animate-pulse rounded-md bg-zinc-700">{dailyTotal}</span>
-      </div>
-
-      <div className="col-span-2 flex flex-col justify-around rounded-xl bg-zinc-100 py-2 px-4 shadow-md dark:bg-zinc-900 sm:col-span-1">
-        <span className="text-sm dark:text-zinc-400">All Time Since Today</span>
-        <span className="text-lg font-semibold">{allTimeSinceToday}</span>
-      </div>
+      {renderItem('Best Day', bestDay)}
+      {renderItem('Daily Average', dailyAverage)}
+      {renderItem('Last Week', dailyTotal)}
+      {renderItem('All Time Since Today', allTimeSinceToday)}
     </div>
   );
 }
