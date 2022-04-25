25c25d69e1dc7de5605522fe9cc8f7ace5604bff
Jared <kuil5230@gmail.com>
2022-04-25 11:46:56 +0800

    chore: Update style

--- src/helpers/mdx-components.helper.js
+++ src/helpers/mdx-components.helper.js
@@ -4,7 +4,7 @@ import CodeBlock from '@/components/CodeBlock';
 import PostImage from '@/components/PostImage';
 
 export const COMPONENTS = {
-  h2: (props) => <h2 {...props} className="pt-2 text-2xl font-semibold" />,
+  h2: (props) => <h2 {...props} className="pt-2 text-xl font-semibold sm:text-2xl" />,
   h3: (props) => <h3 {...props} className="pt-2 text-xl font-semibold" />,
   QuoteHeading: (props) => <h2 {...props} className="mt-2 mb-10 text-xl" />,
   ContentHeading,
