df999370f3c89ef476112fb752cc27786f5e146f
Jared <kuil5230@gmail.com>
2022-05-14 13:20:08 +0800

    chore: Upgrade package versions

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import { PackageIcon } from '@/components/FeatherIcons';
 
 export default function HomePage() {
   return (
-    <div className="flex h-3/4 flex-col items-start gap-6 md:flex-row-reverse md:items-center">
-      <div className="flex justify-center md:flex-1">
+    <div className="flex flex-col items-start gap-6 md:flex-row-reverse md:pt-32">
+      <div className="flex justify-center pt-6 md:flex-1">
         <div className="group relative h-[200px] w-[200px]">
           <Image src={avatar} alt="Jared" width={200} height={200} className="z-10 rounded-md" />
           <div className="absolute -right-4 -bottom-4 flex h-[200px] w-[200px] items-center justify-center rounded-md bg-gradient-to-r from-violet-700 via-cyan-700 to-sky-700 transition-all duration-200 ease-in-out group-hover:-right-3 group-hover:-bottom-3">
@@ -23,7 +23,7 @@ export default function HomePage() {
         <div className="flex flex-col gap-4 pt-6">
           <h1 className="text-4xl">Jared</h1>
           <p className="text-zinc-700 dark:text-zinc-400">Front-End Developer</p>
-          <hr className="my-2 w-20 border border-zinc-300 dark:border-zinc-700" />
+          <hr className="w-20 border border-zinc-300 dark:border-zinc-700" />
 
           <p className="text-zinc-700 dark:text-zinc-400">
             I&#39;m <b>Jared</b>, a Front-end Developer working with <b>React</b> and <b>Next.js</b>{' '}
@@ -56,9 +56,7 @@ export default function HomePage() {
         <ul className="flex items-center gap-4 py-8">
           {contact.map((item) => (
             <Link key={item.link} href={item.link} aria-label={item.label}>
-              <li className="flex items-center gap-2 transition duration-200 ease-in-out hover:text-zinc-500 dark:hover:text-zinc-400">
-                {item.icon}
-              </li>
+              <li className="flex items-center gap-2">{item.icon}</li>
             </Link>
           ))}
         </ul>
