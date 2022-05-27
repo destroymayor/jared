8cb59c51b47e644f14f13efb955d464e7e47468c
Jared <jared@tagtoo.com>
2022-05-27 12:11:44 +0800

    chore: Update style

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -11,8 +11,11 @@ export default function CodeBlock({ children, className }) {
   return (
     <Highlight {...defaultProps} code={children} theme={vsDark} language={language}>
       {({ tokens, getLineProps, getTokenProps }) => (
-        <div className="my-5 flex flex-col items-start rounded-xl bg-zinc-700 px-2 dark:border dark:border-zinc-700 dark:bg-black">
-          <pre ref={textInput} className="w-full overflow-auto px-2 pt-6 pb-3">
+        <div className="relative my-10 flex flex-col items-start rounded-xl border border-zinc-700 bg-zinc-700 px-2 dark:border-zinc-700 dark:bg-black">
+          <span className="absolute -top-[34px] right-10 rounded-t-lg border border-zinc-700 bg-zinc-700 px-3 py-1 text-zinc-400 dark:border-zinc-700 dark:bg-black">
+            {language?.toUpperCase() ?? 'unknown'}
+          </span>
+          <pre ref={textInput} className="w-full overflow-auto pt-6 pb-3">
             {tokens.map((line, i) => (
               <div className="table-row" key={i} {...getLineProps({ line, key: i })}>
                 <span className="table-cell w-6 min-w-[24px] select-none text-right text-zinc-400 dark:text-zinc-600">
