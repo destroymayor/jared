1f3dfe770c3eb1bbd8b49725929d06385d241e5b
Jared <jared@tagtoo.com>
2022-05-28 15:38:55 +0800

    chore: Update style

--- src/components/ContentHeading/ContentHeading.js
+++ src/components/ContentHeading/ContentHeading.js
@@ -1,17 +1,24 @@
-import Link from '@/components/Link';
+import clsx from 'clsx';
+
+import Link from 'next/link';
 import { LinkIcon } from '@/components/FeatherIcons';
 
 export default function ContentHeading(props) {
-  const { children } = props;
+  const { children, className } = props;
 
   const getHeadingId = children?.toLowerCase().replace(new RegExp(' ', 'g'), '-');
 
   return (
-    <h2 {...props} aria-hidden id={getHeadingId} className="group text-2xl">
-      <Link className="flex items-center gap-x-2" href={`#${getHeadingId}`}>
+    <Link passHref href={`#${getHeadingId}`}>
+      <h2
+        {...props}
+        aria-hidden
+        id={getHeadingId}
+        className={clsx('group flex cursor-pointer items-center gap-x-2 text-2xl', className)}
+      >
         {children}
-        <LinkIcon className="invisible h-6 w-6 transition duration-150 ease-in-out group-hover:visible " />
-      </Link>
-    </h2>
+        <LinkIcon className="invisible h-5 w-5 transition duration-150 ease-in-out group-hover:visible" />
+      </h2>
+    </Link>
   );
 }
