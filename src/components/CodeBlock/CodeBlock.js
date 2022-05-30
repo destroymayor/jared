2e5b5720b8f5ca046812b369b1051306fade9422
Jared <kuil5230@gmail.com>
2022-05-30 17:53:13 +0800

    chore: Update style

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -16,14 +16,14 @@ export default function CodeBlock({ children, className }) {
   return (
     <Highlight {...defaultProps} code={children} theme={codeBlockTheme} language={language}>
       {({ tokens, getLineProps, getTokenProps }) => (
-        <div className="relative my-10 flex flex-col items-start rounded-xl border border-zinc-100 bg-zinc-100 px-2 dark:border-zinc-700 dark:bg-black">
-          <span className="absolute -top-[34px] right-10 rounded-t-lg border border-zinc-100 bg-zinc-100 px-3 py-1 text-zinc-700 dark:border-zinc-700 dark:bg-black dark:text-zinc-400">
+        <div className="relative my-10 mx-[-10px] flex flex-col items-start rounded-xl border border-zinc-300 bg-zinc-100 px-2 dark:border-zinc-700 dark:bg-black sm:mx-0">
+          <span className="absolute -top-[34px] right-10 rounded-t-lg border border-zinc-300 bg-zinc-100 px-3 py-1 text-zinc-700 dark:border-zinc-700 dark:bg-black dark:text-zinc-400">
             {language?.toUpperCase() ?? 'unknown'}
           </span>
           <pre ref={textInput} className="w-full overflow-auto pt-6 pb-3">
             {tokens.map((line, i) => (
               <div className="table-row" key={i} {...getLineProps({ line, key: i })}>
-                <span className="table-cell w-6 min-w-[24px] select-none text-right text-zinc-400 dark:text-zinc-600">
+                <span className="sticky left-0 table-cell w-6 min-w-[24px] select-none bg-zinc-100 pr-2 text-right text-zinc-400 dark:bg-black dark:text-zinc-600">
                   {i + 1}
                 </span>
                 <span className="table-cell pl-4">
