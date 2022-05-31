6cccb84d1c7c599d6dd6f2d37c441ce887fce394
Jared <kuil5230@gmail.com>
2022-05-31 17:23:28 +0800

    chore: Update bgm player component

--- src/components/FeatherIcons/index.js
+++ src/components/FeatherIcons/index.js
@@ -213,6 +213,21 @@ export const ToolIcon = (props) => (
   </FeatherIcon>
 );
 
+export const Volume2Icon = (props) => (
+  <FeatherIcon {...props}>
+    <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+    <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>
+  </FeatherIcon>
+);
+
+export const VolumeXIcon = (props) => (
+  <FeatherIcon {...props}>
+    <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+    <line x1="23" y1="9" x2="17" y2="15"></line>
+    <line x1="17" y1="9" x2="23" y2="15"></line>
+  </FeatherIcon>
+);
+
 export const XIcon = (props) => (
   <FeatherIcon {...props}>
     <line x1="18" y1="6" x2="6" y2="18"></line>
