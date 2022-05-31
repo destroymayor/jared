f7dfa43af2f4ba4794190e9a0d188841649d2bff
Jared <kuil5230@gmail.com>
2022-05-31 12:10:54 +0800

    chore: Update style

--- src/components/NowPlaying/PlayingBars.js
+++ src/components/NowPlaying/PlayingBars.js
@@ -1,60 +1,70 @@
 import { motion } from 'framer-motion';
 
+const bars = [
+  {
+    animate: {
+      scaleY: [1.0, 1.5, 1.0],
+      translateY: ['0rem', '-0.082rem', '0rem'],
+    },
+    transition: {
+      duration: 1,
+      easings: 'easeInOut',
+      repeat: Infinity,
+    },
+    className: 'mr-[1px] h-2 w-1 bg-zinc-800 opacity-75 dark:bg-zinc-500',
+  },
+  {
+    animate: {
+      scaleY: [1.0, 3, 1.0],
+      translateY: ['0rem', '-0.083rem', '0rem'],
+    },
+    transition: {
+      duration: 1.5,
+      easings: 'easeInOut',
+      repeat: Infinity,
+      repeatDelay: 0.2,
+    },
+    className: 'mr-[1px] h-1 w-1 bg-zinc-800 dark:bg-zinc-500',
+  },
+  {
+    animate: {
+      scaleY: [1.0, 1.5, 1.0],
+      translateY: ['0rem', '0.37rem', '0rem'],
+    },
+    transition: {
+      duration: 1,
+      easings: 'easeInOut',
+      repeat: Infinity,
+      repeatDelay: 0.2,
+    },
+    className: 'mr-[1px] h-3 w-1 bg-zinc-800 opacity-80 dark:bg-zinc-500',
+  },
+  {
+    animate: {
+      scaleY: [1.0, 1.5, 1.0],
+      translateY: ['0rem', '0.37rem', '0rem'],
+    },
+    transition: {
+      duration: 1.5,
+      easings: 'easeInOut',
+      repeat: Infinity,
+      repeatDelay: 0.3,
+    },
+    className: 'h-3 w-1 bg-zinc-800 dark:bg-zinc-500',
+  },
+];
+
 export default function PlayingBars() {
   return (
     <div className="relative flex w-auto transform items-end overflow-hidden pt-1 lg:rotate-90 lg:pt-0">
-      <motion.span
-        animate={{
-          scaleY: [1.0, 1.5, 1.0],
-          translateY: ['0rem', '-0.082rem', '0rem'],
-        }}
-        transition={{
-          duration: 1,
-          easings: 'easeInOut',
-          repeat: Infinity,
-        }}
-        className="mr-[1px] h-2 w-1 bg-zinc-800 opacity-75 dark:bg-zinc-500"
-      />
-      <motion.span
-        animate={{
-          scaleY: [1.0, 3, 1.0],
-          translateY: ['0rem', '-0.083rem', '0rem'],
-        }}
-        transition={{
-          duration: 1.5,
-          easings: 'easeInOut',
-          repeat: Infinity,
-          repeatDelay: 0.2,
-        }}
-        className="mr-[1px] h-1 w-1 bg-zinc-800 dark:bg-zinc-500"
-      />
-      <motion.span
-        animate={{
-          scaleY: [1.0, 1.5, 1.0],
-          translateY: ['0rem', '0.37rem', '0rem'],
-        }}
-        transition={{
-          duration: 1,
-          easings: 'easeInOut',
-          repeat: Infinity,
-          repeatDelay: 0.2,
-        }}
-        className="mr-[1px] h-3 w-1 bg-zinc-800 opacity-80 dark:bg-zinc-500"
-      />
-
-      <motion.span
-        animate={{
-          scaleY: [1.0, 0.5, 1.0],
-          translateY: ['0rem', '0.37rem', '0rem'],
-        }}
-        transition={{
-          duration: 1.5,
-          easings: 'easeInOut',
-          repeat: Infinity,
-          repeatDelay: 0.3,
-        }}
-        className="h-3 w-1 bg-zinc-800 dark:bg-zinc-500"
-      />
+      {bars.map((bar, index) => (
+        <motion.span
+          key={`${index}`}
+          animate={bar.animate}
+          transition={bar.transition}
+          className={bar.className}
+        />
+      ))}
     </div>
   );
 }
