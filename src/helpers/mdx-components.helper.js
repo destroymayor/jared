fa44dd450ed14c2a4c169d944da0698f57137549
Jared <kuil5230@gmail.com>
2022-07-03 15:19:20 +0800

    chore: Update Mdx components style

--- src/helpers/mdx-components.helper.js
+++ src/helpers/mdx-components.helper.js
@@ -6,7 +6,7 @@ import PostImage from '@/components/PostImage';
 export const COMPONENTS = {
   h2: ContentHeading,
   h3: ContentHeading,
-  ul: (props) => <ul {...props} className="flex list-disc flex-col gap-4 py-4 pl-7" />,
+  ul: (props) => <ul {...props} className="flex list-disc flex-col gap-3 pb-4 pl-7" />,
   li: (props) => <li {...props} className="px-2" />,
   a: (props) => <a {...props} className="hover:underline" />,
   QuoteHeading: (props) => <h2 {...props} className="mt-2 mb-10 text-xl" />,
