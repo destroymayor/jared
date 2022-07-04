b124f575d43cea97bd37dab7e95fbfdc4cc78c9e
Jared <jared@tagtoo.com>
2022-07-04 12:09:06 +0800

    chore: update command palette style

--- src/components/CommandPalette/CommandMenu.js
+++ src/components/CommandPalette/CommandMenu.js
@@ -1,4 +1,5 @@
 import { useCallback, Fragment } from 'react';
+import { motion } from 'framer-motion';
 
 import Tabs from '@/components/Tabs';
 
@@ -18,31 +19,32 @@ export default function CommandMenu(props) {
   }
 
   return (
-    <Tabs
-      direction="vertical"
+    <motion.div
       className="overflow-y-auto transition-[height] duration-100 will-change-[height] [transition-timing-function:ease]"
       style={{ height: getMenuContainerHeight < 340 ? getMenuContainerHeight : 340 }}
     >
-      {options.map((option) => (
-        <Fragment key={option.title}>
-          {option.children.length > 0 && (
-            <>
-              <span className="p-2 text-xs text-zinc-400">{option.title}</span>
-              {option.children.map((child) => (
-                <Tabs.Tab key={child.title} value={child}>
-                  <button
-                    className="flex w-full items-center gap-3 p-2 text-zinc-600 transition-colors duration-200 ease-in-out hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-white"
-                    onClick={() => handleSelect(child)}
-                  >
-                    <span className="h-5 w-5">{child.icon}</span>
-                    <span>{child.title}</span>
-                  </button>
-                </Tabs.Tab>
-              ))}
-            </>
-          )}
-        </Fragment>
-      ))}
-    </Tabs>
+      <Tabs direction="vertical">
+        {options.map((option) => (
+          <Fragment key={option.title}>
+            {option.children.length > 0 && (
+              <>
+                <span className="p-2 text-xs text-zinc-400">{option.title}</span>
+                {option.children.map((child) => (
+                  <Tabs.Tab key={child.title} value={child}>
+                    <button
+                      className="flex w-full items-center gap-3 p-2 text-zinc-600 transition-colors duration-200 ease-in-out hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-white"
+                      onClick={() => handleSelect(child)}
+                    >
+                      <span className="h-5 w-5">{child.icon}</span>
+                      <span>{child.title}</span>
+                    </button>
+                  </Tabs.Tab>
+                ))}
+              </>
+            )}
+          </Fragment>
+        ))}
+      </Tabs>
+    </motion.div>
   );
 }
