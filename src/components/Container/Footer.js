2a58c9aca58c6e5d95a6b3d677fa1af420928bf6
Jared <jared@tagtoo.com>
2022-07-23 21:25:27 +0800

    chore: Update style

--- src/components/Container/Footer.js
+++ src/components/Container/Footer.js
@@ -1,7 +1,5 @@
 import useSWR from 'swr';
 
-import contact from '@/data/contact';
-
 import NowPlaying from '@/components/NowPlaying';
 import Link from '@/components/Link';
 import { StartIcon } from '@/components/Icons';
@@ -12,46 +10,25 @@ export default function Footer() {
   });
 
   return (
-    <footer className="mt-12 flex w-11/12 flex-col gap-6 border-t border-dashed border-zinc-300 py-6 dark:border-zinc-700 sm:max-w-[686px]">
-      <div className="relative flex flex-col-reverse gap-6">
-        <ul className="flex items-center gap-6 lg:fixed lg:bottom-0 lg:left-12 lg:flex-col lg:gap-4">
-          {contact.map((item) => (
-            <li key={item.link}>
-              <Link
-                className="flex items-center gap-2 text-zinc-500 transition duration-200 ease-in-out hover:-translate-y-1 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-50 lg:p-1"
-                href={item.link}
-                aria-label={item.label}
-              >
-                <span className="h-6 w-6">{item.icon}</span>
-              </Link>
-            </li>
-          ))}
-          <li className="hidden h-24 border-l border-zinc-300 dark:border-zinc-700 lg:block"></li>
-        </ul>
+    <footer className="mt-12 flex w-11/12 flex-col gap-4 border-t border-dashed border-zinc-300 py-6 dark:border-zinc-700 sm:max-w-[686px]">
+      <NowPlaying />
 
-        <div className="flex items-center lg:fixed lg:bottom-0 lg:right-12 lg:flex-col">
-          <NowPlaying />
-          <div className="mt-4 hidden h-24 border-l border-zinc-300 dark:border-zinc-700 lg:block"></div>
-        </div>
-      </div>
-
-      <div className="flex flex-col flex-wrap items-start justify-between gap-4 dark:text-zinc-400 lg:items-center lg:gap-2">
+      <div className="flex flex-col items-start gap-2 dark:text-zinc-400">
         <Link
           href="https://github.com/destroymayor/jared"
-          className="flex items-center transition duration-200 ease-in-out dark:hover:text-zinc-50"
+          className="flex items-center gap-1 transition duration-200 ease-in-out dark:hover:text-zinc-50"
         >
           {githubInfo?.stargazers_count && (
-            <div className="flex items-center gap-1 px-1">
+            <div className="flex items-center gap-1">
               <StartIcon className="h-4 w-4" />
               <span>{githubInfo?.stargazers_count?.toLocaleString()}</span>
               <span className="text-zinc-400 dark:text-zinc-600">/</span>
             </div>
           )}
-
           <span className="text-sm">Designed & Built by Jared</span>
         </Link>
 
-        <span className="flex items-center gap-2 text-sm sm:text-base">
+        <div className="flex items-center gap-2 text-sm sm:text-base">
           <span className="text-lg">©</span>
           <span>{new Date().getFullYear()}</span>
           <span className="text-zinc-400 dark:text-zinc-600">-</span>
@@ -60,7 +37,7 @@ export default function Footer() {
             <span className="text-zinc-400 dark:text-zinc-600">/</span>
             <span>him</span>
           </span>
-        </span>
+        </div>
       </div>
     </footer>
   );
--- src/components/NowPlaying/NowPlaying.js
+++ src/components/NowPlaying/NowPlaying.js
@@ -2,7 +2,6 @@ import Image from 'next/future/image';
 
 import useSWR from 'swr';
 
-import clsx from 'clsx';
 import PlayingBars from '@/components/NowPlaying/PlayingBars';
 import Link from '@/components/Link';
 import { SpotifySolidIcon } from '@/components/Icons';
@@ -10,61 +9,44 @@ import { SpotifySolidIcon } from '@/components/Icons';
 export default function NowPlaying() {
   const { data } = useSWR('/api/spotify/now-playing');
 
-  const isLoading = !data;
   const isPlaying = data?.isPlaying;
 
-  const songUrl = data?.songUrl;
+  const songUrl = data?.songUrl ?? '#';
   const songTitle = data?.title;
   const artist = data?.artist;
   const album = data?.album;
   const albumImageUrl = data?.albumImageUrl;
 
   return (
-    <div className="flex flex-row-reverse items-center gap-3 lg:flex-col">
-      <div
-        className={clsx(
-          'flex flex-col-reverse items-start lg:items-center',
-          isLoading ? 'invisible' : 'visible'
-        )}
-      >
-        <div
-          className={clsx(
-            'flex w-52 flex-col truncate text-sm sm:w-auto',
-            isPlaying ? 'lg:hidden' : 'hidden'
-          )}
-        >
-          <Link href={songUrl ?? '#'} className="hover:underline">
-            {songTitle}
-          </Link>
-          <span className="text-zinc-500 dark:text-zinc-400">{artist}</span>
-        </div>
-
-        <div
-          className={clsx(
-            'transform dark:text-zinc-400 lg:w-auto lg:-rotate-180 lg:[writing-mode:vertical-lr]',
-            isPlaying ? 'hidden' : ''
-          )}
-        >
-          Not Playing
-        </div>
-
-        {isPlaying && <PlayingBars />}
-      </div>
-
+    <div className="flex  items-center gap-3">
       {isPlaying && albumImageUrl ? (
         <Link href={songUrl} className="flex h-[60px] w-[60px]">
           <Image className="rounded-md" alt={album} src={albumImageUrl} width={60} height={60} />
         </Link>
       ) : (
-        <div
-          className={clsx(
-            'flex h-[60px] w-[60px] items-center justify-center rounded-lg border border-zinc-200 dark:border-zinc-800 lg:border-0',
-            isLoading ? 'invisible' : 'visible'
-          )}
-        >
+        <div className="flex items-center justify-center rounded-lg border border-zinc-200 dark:border-zinc-800">
           <SpotifySolidIcon className="h-7 w-7" />
         </div>
       )}
+
+      <div className="flex flex-col items-start justify-center">
+        {isPlaying ? (
+          <>
+            <PlayingBars />
+            <div className="flex w-52 flex-col truncate text-sm sm:w-auto">
+              <Link href={songUrl} className="hover:underline">
+                {songTitle}
+              </Link>
+              <span className="text-zinc-500 dark:text-zinc-400">{artist}</span>
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center">
+            <span>Not Playing</span>
+            <span className="text-sm dark:text-zinc-400"> - Spotify</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
