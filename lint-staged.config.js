01a143eaf7e3a66738ab1d49d938d7cf134495b5
Jared <jared@tagtoo.com>
2022-08-11 15:50:26 +0800

    chore: Update lin staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'tsc -p tsconfig.json --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
