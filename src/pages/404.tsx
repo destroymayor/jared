c97e0cf3b664e8df60a12ac928dc957c3fd7bd17
Jared <jared@tagtoo.com>
2022-08-16 15:36:55 +0800

    chore: Update 404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,14 @@ import type { ReactElement } from 'react';
 import type { NextPageWithLayout } from './_app';
 
 import Container from '@/components/Container';
-import { FrownIcon } from '@/components/Icons';
 
 const Custom404: NextPageWithLayout = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-y-10">
-      <FrownIcon className="h-20 w-20" />
-      <p className="text-lg">This page cannot be found.</p>
+    <div className="flex h-[600px] flex-col items-center justify-center">
+      <h1 className="bg-gradient-to-r from-sky-600 via-sky-500 to-sky-500 bg-clip-text text-7xl font-extrabold text-transparent dark:from-sky-700 dark:via-sky-600 dark:to-sky-500">
+        404
+      </h1>
+      <p className="text-lg pt-10">This page cannot be found.</p>
     </div>
   );
 };
