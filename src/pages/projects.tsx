cdf544db44ac9f16e6ccaae48afd2d4ef1654b54
Jared <jared@tagtoo.com>
2022-08-16 12:26:25 +0800

    chore: Update style

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -18,68 +18,57 @@ const description = `Internet thingies built with React, Next.js.`;
 const Page: NextPageWithLayout = () => {
   return (
     <ul className="flex flex-col gap-10">
-      {projects.map(
-        (project: {
-          tags: string[];
-          title: string;
-          description: string;
-          release_year: number;
-          image: string;
-          links: {
-            demo: string;
-            repo: string;
-          };
-        }) => {
-          const { tags, title, description, release_year, image, links } = project;
+      {projects.map((project) => {
+        const { tags, title, description, release_year, image, links } = project;
 
-          return (
-            <li key={title}>
-              <FadeInContainer className="-mx-4">
-                <Image
-                  className="rounded-md object-contain"
-                  alt={title}
-                  src={image}
-                  width={714}
-                  height={429}
-                />
-              </FadeInContainer>
+        return (
+          <li key={title}>
+            <FadeInContainer className="-mx-4">
+              <Image
+                priority
+                className="rounded-md object-contain"
+                alt={title}
+                src={image}
+                width={714}
+                height={429}
+              />
+            </FadeInContainer>
 
-              <div className="flex flex-col gap-2 sm:gap-4 sm:px-2">
-                <div className="flex flex-wrap justify-between gap-4">
-                  <div className="flex flex-wrap items-center gap-2">
-                    {tags.map((tag) => (
-                      <Tag key={tag} label={tag} />
-                    ))}
-                  </div>
-
-                  <span className="text-zinc-600 dark:text-zinc-400">{release_year}</span>
+            <div className="flex flex-col gap-2 sm:gap-4 sm:px-2">
+              <div className="flex flex-wrap justify-between gap-4">
+                <div className="flex flex-wrap items-center gap-2">
+                  {tags.map((tag) => (
+                    <Tag key={tag} label={tag} />
+                  ))}
                 </div>
 
-                <h2 className="text-xl font-semibold">{title}</h2>
-                <p className="text-sm dark:text-zinc-400">{description}</p>
-
-                <div className="flex items-center gap-2 text-sm">
-                  <ExternalLink
-                    href={links.demo}
-                    className="flex items-center gap-2 rounded-md p-2 font-bold hover:bg-sky-600/10 dark:text-zinc-400 dark:hover:text-zinc-100"
-                  >
-                    <GlobeIcon className="h-5 w-5" />
-                    <span>Demo</span>
-                  </ExternalLink>
-
-                  <ExternalLink
-                    href={links.repo}
-                    className="flex items-center gap-2 rounded-md p-2 font-bold hover:bg-sky-600/10 dark:text-zinc-400 dark:hover:text-zinc-100"
-                  >
-                    <GithubOutlineIcon className="h-5 w-5" />
-                    <span>Repo</span>
-                  </ExternalLink>
-                </div>
+                <span className="text-zinc-600 dark:text-zinc-400">{release_year}</span>
               </div>
-            </li>
-          );
-        }
-      )}
+
+              <h2 className="text-xl font-semibold">{title}</h2>
+              <p className="text-sm dark:text-zinc-400">{description}</p>
+
+              <div className="flex items-center gap-2 text-sm">
+                <ExternalLink
+                  href={links.demo}
+                  className="flex items-center gap-2 rounded-md p-2 font-bold hover:bg-sky-600/10 dark:text-zinc-400 dark:hover:text-zinc-100"
+                >
+                  <GlobeIcon className="h-5 w-5" />
+                  <span>Demo</span>
+                </ExternalLink>
+
+                <ExternalLink
+                  href={links.repo}
+                  className="flex items-center gap-2 rounded-md p-2 font-bold hover:bg-sky-600/10 dark:text-zinc-400 dark:hover:text-zinc-100"
+                >
+                  <GithubOutlineIcon className="h-5 w-5" />
+                  <span>Repo</span>
+                </ExternalLink>
+              </div>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 };
