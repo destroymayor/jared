a7714c3e638bc211d987c49dbe856b6ac5859b3e
Jared <jared@tagtoo.com>
2022-08-23 13:31:23 +0800

    chore: Update style

--- src/layouts/snippets.tsx
+++ src/layouts/snippets.tsx
@@ -1,9 +1,7 @@
 import type { ReactNode } from 'react';
-import Link from 'next/link';
 
 import Container from '@/components/Container';
 import Giscus from '@/components/Giscus';
-import { ChevronLeftIcon } from '@/components/Icons';
 
 type Props = {
   title: string;
@@ -25,16 +23,9 @@ export default function SnippetLayout(props: Props) {
 
   return (
     <Container title={title} description={description}>
-      <header className="flex flex-col items-start gap-4">
-        <Link href="/snippets">
-          <span className="flex cursor-pointer items-center gap-2 font-semibold text-zinc-600 transition duration-200 ease-in-out hover:text-sky-800 dark:text-zinc-400 dark:hover:text-sky-600">
-            <ChevronLeftIcon className="h-4 w-4" />
-            <span className="text-sm">Back to snippets</span>
-          </span>
-        </Link>
-
-        <h1 className="text-2xl pt-4 font-semibold sm:text-3xl">{title}</h1>
-        <p className="text-zinc-600 dark:text-zinc-400">{description}</p>
+      <header className="pt-2 md:pt-8">
+        <h1 className="py-2 text-4xl font-extrabold">{title}</h1>
+        <p className="pt-2 text-zinc-600 dark:text-zinc-400">{description}</p>
       </header>
 
       <div className="my-6 border-b border-dashed border-zinc-300 dark:border-zinc-700" />
