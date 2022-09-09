fa9ccd869950bd562f3c04c9dd0700fc53971d7c
Jared <jared@tagtoo.com>
2022-09-09 22:55:26 +0800

    chore: Upgrade package versions

--- next.config.js
+++ next.config.js
@@ -12,11 +12,4 @@ module.exports = {
       'wakatime.com',
     ],
   },
-  experimental: {
-    legacyBrowsers: false,
-    browsersListForSwc: true,
-    images: {
-      allowFutureImage: true,
-    },
-  },
 };
