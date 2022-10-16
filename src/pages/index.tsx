4bba8e90b4b4798042d62c2ef9185daac032d415
Jared <jared@tagtoo.com>
2022-10-16 18:49:54 +0800

    chore(home): Update style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import ExternalLink from '@/components/ExternalLink';
 
 const Page: NextPageWithLayout = () => {
   return (
-    <div className="flex flex-col gap-3 sm:pt-10">
+    <div className="flex flex-col gap-3 pt-12 sm:pt-24">
       <h1 className="text-4xl font-extrabold">Jared</h1>
       <h2 className="text-zinc-700 dark:text-zinc-400">Front-End Developer</h2>
 
@@ -17,13 +17,20 @@ const Page: NextPageWithLayout = () => {
         <b>Next.js</b> who enjoys building great quality and great user experience products.
       </p>
 
-      <p className="text-sm leading-7 text-zinc-600 dark:text-zinc-400">
+      <p className="flex items-end text-sm leading-7 text-zinc-600 dark:text-zinc-400">
         Currently working at{' '}
-        <ExternalLink
-          href="https://www.pinkoi.com"
-          className="border-b border-zinc-300 pb-[2px] font-bold text-zinc-800 transition duration-300 ease-in-out hover:border-zinc-500 dark:border-zinc-500 dark:text-zinc-50 dark:hover:border-zinc-300"
-        >
-          Pinkoi
+        <ExternalLink href="https://www.pinkoi.com" className="flex items-end gap-2 pl-2">
+          <svg height={28} width={28} viewBox="0 0 36 36">
+            <path
+              d="M27.6 36H8.4A8.4 8.4 0 0 1 0 27.6V8.4A8.4 8.4 0 0 1 8.4 0h19.2A8.4 8.4 0 0 1 36 8.4v19.2a8.4 8.4 0 0 1-8.4 8.4"
+              fill="#003354"
+            />
+            <path
+              d="M19.448 6.827c-4.542-.032-8.258 3.802-8.258 8.344v7.852c0 2.127-1.322 3.928-3.15 4.55a.42.42 0 0 0-.069.762l2.32 1.34c.394.227.884.192 1.248-.08a8.187 8.187 0 0 0 3.295-6.572V15.13c0-2.505 1.98-4.62 4.485-4.657a4.555 4.555 0 0 1 4.625 4.625c-.037 2.505-2.152 4.485-4.657 4.485H17.27a.563.563 0 0 0-.563.562v2.52c0 .31.252.562.563.562h1.975c4.542 0 8.375-3.716 8.344-8.258a8.2 8.2 0 0 0-8.14-8.141"
+              fill="#ee847d"
+            />
+          </svg>
+          <span className="font-bold text-zinc-800 dark:text-zinc-50">Pinkoi</span>
         </ExternalLink>
         .
       </p>
