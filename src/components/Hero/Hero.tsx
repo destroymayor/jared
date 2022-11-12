9abd2c2af6d36f2328e4e7f990e6afab587cce07
Jared Chen <kuil5230@gmail.com>
2022-11-12 23:53:30 +0800

    chore: Update style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,24 @@
+import clsx from 'clsx';
+
 type Props = {
   title: string;
   description: string;
+  dashedDisplay?: boolean;
 };
 
 export default function Hero(props: Props) {
-  const { title, description } = props;
+  const { title, description, dashedDisplay = true } = props;
 
   return (
     <div className="pt-2 md:pt-8">
       <h1 className="py-2 text-4xl font-extrabold">{title}</h1>
       <p className="max-w-[60ch] pt-2 dark:text-zinc-400">{description}</p>
-      <div className="my-6 border-b border-dashed border-zinc-300 dark:border-zinc-700" />
+      <div
+        className={clsx(
+          'my-6',
+          dashedDisplay && ' border-b border-dashed border-zinc-300 dark:border-zinc-700'
+        )}
+      />
     </div>
   );
 }
--- src/pages/photos.tsx
+++ src/pages/photos.tsx
@@ -1,14 +1,11 @@
 import type { ReactElement } from 'react';
 import type { NextPageWithLayout } from './_app';
 import Image from 'next/image';
-import Link from 'next/link';
 
 import useSWR from 'swr';
-
 import { motion } from 'framer-motion';
 import useBlurhashToBase64 from '@/hooks/use-blur-hash-to-base64.hook';
 
-import { DownloadIcon, LinkIcon } from '@/components/Icons';
 import Container from '@/components/Container';
 import Hero from '@/components/Hero';
 
@@ -49,7 +46,7 @@ const Page: NextPageWithLayout = () => {
   };
 
   return (
-    <ul className="relative left-1/2 right-1/2 -mx-[50vw] grid min-h-screen w-screen gap-2 px-2 [grid-template-columns:repeat(auto-fill,minmax(100%,1fr))] sm:[grid-template-columns:repeat(auto-fill,minmax(440px,1fr))]">
+    <ul className="relative left-1/2 right-1/2 -mx-[50vw] grid min-h-screen w-[99.5vw] gap-2 pr-2 pl-4 [grid-template-columns:repeat(auto-fill,minmax(100%,1fr))] sm:[grid-template-columns:repeat(auto-fill,minmax(440px,1fr))]">
       {photos?.map((photo) => {
         const blurDataURL = getBlurData({ blur_hash: photo.blur_hash, width: 440, height: 700 });
 
@@ -73,50 +70,6 @@ const Page: NextPageWithLayout = () => {
               width={1080}
               height={400}
             />
-            <div className="absolute left-0 right-0 bottom-0 hidden items-center justify-between rounded-b-md bg-zinc-100 p-2 text-sm text-zinc-600 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-90 dark:bg-zinc-900 dark:text-zinc-400 sm:flex">
-              <div>
-                <span>Photo by </span>
-                <Link
-                  href="https://unsplash.com/@destroymayor"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="underline hover:text-zinc-900 dark:hover:text-zinc-50"
-                >
-                  Jared Chen
-                </Link>
-                <span> / </span>
-                <Link
-                  href="https://unsplash.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="underline hover:text-zinc-900 dark:hover:text-zinc-50"
-                >
-                  Unsplash
-                </Link>
-              </div>
-              <div className="flex items-center gap-2">
-                <Link
-                  href={photo.links.html}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-1 hover:text-zinc-900 dark:hover:text-zinc-50"
-                >
-                  <LinkIcon className="h-4 w-4" />
-                  Link
-                </Link>
-                <span>·</span>
-                <Link
-                  href={photo.links.download}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  download
-                  className="flex items-center gap-1 hover:text-zinc-900 dark:hover:text-zinc-50"
-                >
-                  <DownloadIcon className="h-4 w-4" />
-                  Download
-                </Link>
-              </div>
-            </div>
           </motion.li>
         );
       })}
@@ -127,7 +80,7 @@ const Page: NextPageWithLayout = () => {
 Page.getLayout = function getLayout(page: ReactElement) {
   return (
     <Container title={title} description={description}>
-      <Hero title={title} description={description} />
+      <Hero title={title} description={description} dashedDisplay={false} />
       {page}
     </Container>
   );
