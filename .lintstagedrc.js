e09ebc441df420e5d29bddad30866ec6d4fab305
Jared Chen <kuil5230@gmail.com>
2023-09-23 13:47:13 +0800

    upgrade package version

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,10 @@
+const path = require('path')
+ 
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+ 
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'tsc -p tsconfig.json --noEmit',
-
-  // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `pnpm eslint --fix ${filenames.join(' ')}`,
-    `pnpm prettier --write ${filenames.join(' ')}`,
-  ],
-
-  // Format MarkDown and JSON
-  '**/*.(md|mdx|json)': (filenames) => `pnpm prettier --write ${filenames.join(' ')}`,
-};
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
