a816985eeb5804bf6aca79e142cc50b89ac4ddbe
Jared Chen <kuil5230@gmail.com>
2024-09-28 22:31:30 +0800

    update style

--- src/components/MDX/CodeBlock.tsx
+++ src/components/MDX/CodeBlock.tsx
@@ -25,7 +25,7 @@ export default function CodeBlock(props: { className?: string; children: any }) 
     return (
         <Highlight code={children} theme={codeBlockTheme} language={language}>
             {({ tokens, getLineProps, getTokenProps }) => (
-                <div className="relative -mx-4 my-20 flex flex-col items-start border border-zinc-100 bg-zinc-100 px-2 dark:border-zinc-900 dark:bg-zinc-900 sm:mx-[calc(100vw-103vw)] sm:rounded-xl md:mx-0">
+                <div className="relative -mx-4 my-12 flex flex-col items-start border border-zinc-100 bg-zinc-100 px-2 dark:border-zinc-900 dark:bg-zinc-900 sm:mx-[calc(100vw-103vw)] sm:rounded-xl md:mx-0">
                     <span className="absolute -top-[33px] right-10 rounded-t-lg border-b border-b-zinc-100 bg-zinc-100 px-4 py-1 font-bold text-zinc-700 dark:border-zinc-900 dark:bg-zinc-900 dark:text-zinc-400">
                         {language?.toUpperCase() ?? 'unknown'}
                     </span>
