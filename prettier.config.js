725bf53904b42fa02946193ef8ee01172f6e5cae
Jared Chen <kuil5230@gmail.com>
2024-12-15 15:40:00 +0800

    upgrade package version

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    arrowParens: 'always',
-    tabWidth: 4,
-    printWidth: 90,
-    singleQuote: true,
-    jsxSingleQuote: false,
-    trailingComma: 'es5',
-    semi: true,
-};
