9f276a688c5d7b3199f57f3fbc5aacf692b775b7
Jared Chen <jared.chen@pinkoi.com>
2024-12-24 18:32:30 +0800

    wip

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -16,38 +16,47 @@ import MotionValue from './MotionValue';
 const list = [
     {
         title: 'Component',
+        value: 'component',
         component: <Component />,
     },
     {
         title: 'Transition',
+        value: 'transition',
         component: <Transition />,
     },
     {
         title: 'Keyframers',
+        value: 'keyframers',
         component: <Keyframers />,
     },
     {
         title: '(WIP) 手勢',
+        value: 'gesture',
         component: null,
     },
     {
         title: '(WIP) Scroll',
+        value: 'scroll',
         component: null,
     },
     {
         title: 'AnimatePresence',
+        value: 'animatePresence',
         component: <AnimatePresence />,
     },
     {
         title: '(WIP)Propagation',
+        value: 'propagation',
         component: null,
     },
     {
         title: 'MotionValue',
+        value: 'motionValue',
         component: <MotionValue />,
     },
     {
         title: '(WIP)hooks',
+        value: 'hooks',
         component: null,
     },
 ];
@@ -61,18 +70,20 @@ export default function Page() {
                 framer-motion，讓你可以直接在 React component props 中操作動畫。
             </p>
 
-            <Accordion type="multiple">
-                {list.map((item) => (
-                    <AccordionItem
-                        value={item.title}
-                        key={item.title}
-                        className="text-2xl"
-                    >
-                        <AccordionTrigger>{item.title}</AccordionTrigger>
-                        <AccordionContent>{item.component}</AccordionContent>
-                    </AccordionItem>
-                ))}
-            </Accordion>
+            <div className='border rounded-md px-4'>
+                <Accordion type="multiple" defaultValue={['component']}>
+                    {list.map((item) => (
+                        <AccordionItem
+                            value={item.value}
+                            key={item.title}
+                            className="text-2xl"
+                        >
+                            <AccordionTrigger>{item.title}</AccordionTrigger>
+                            <AccordionContent>{item.component}</AccordionContent>
+                        </AccordionItem>
+                    ))}
+                </Accordion>
+            </div>
         </div>
     );
 }
