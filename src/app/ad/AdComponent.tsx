505720aa606143d333aa63767615adc27cda6b8b
Jared Chen <jared.chen@pinkoi.com>
2025-01-14 18:53:39 +0800

    wip

--- src/app/ad/AdComponent.tsx
+++ src/app/ad/AdComponent.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React, { useEffect } from 'react';
 import { cn } from '@/lib/utils';
 
 const AdComponent: React.FC<{
@@ -9,16 +8,8 @@ const AdComponent: React.FC<{
 }> = (props) => {
     const { adId, className } = props;
 
-    useEffect(() => {
-        const googletag = (window as any).googletag;
-        googletag.cmd.push(function () {
-            googletag.display(adId);
-        });
-    }, []);
-
     return (
-        <div id={adId} className={cn('min-h-[150px] bg-red-200 text-black', className)}>
-            {adId}
+        <div id={adId} className={cn(' bg-red-200 text-black', className)}>
         </div>
     );
 };
--- src/app/ad/SDK.tsx
+++ src/app/ad/SDK.tsx
@@ -7,47 +7,32 @@ const safeFrameConfig = {
     allowPushExpansion: true,
 };
 
-const SLOT_LIST = [
+const viewId = String(Date.now());
+
+const SLOT_FIXED_LIST = [
     {
-        unit: '/23212980789/listing_rectangle_unit',
-        id: 'listing-0',
-        className: 'listing-container',
+        unit: '/23212980789/brand_unit',
+        class: 'post-bottom-container',
         size: [620, 238],
     },
     {
         unit: '/23212980789/brand_unit',
-        id: 'brand-square-0',
-        className: 'brand-square-container',
+        class: 'below-search-container',
+        size: [386, 521],
+    },
+    {
+        unit: '/23212980789/brand_unit',
+        class: 'panel-top-container',
         size: [386, 356],
     },
     {
         unit: '/23212980789/brand_unit',
-        id: 'brand-rectangle-0',
-        className: 'brand-rectangle-container',
-        size: [726, 150],
-    },
-    {
-        unit: '/23212980789/brand_unit',
-        id: 'brand-rectangle-1',
-        className: 'brand-rectangle-container',
-        size: [726, 150],
-    },
-    {
-        unit: '/23212980789/brand_unit',
-        id: 'brand-rectangle-2',
-        className: 'brand-rectangle-container',
-        size: [726, 150],
-    },
-    {
-        unit: '/23212980789/brand_unit',
-        id: 'brand-rectangle-3',
-        className: 'brand-rectangle-container',
+        class: 'post-inside-container',
         size: [726, 150],
     },
 ];
 
-const SDK = (props: { onCompleted: () => void }) => {
-    const { onCompleted } = props;
+const SDK = () => {
 
     const loadGoogleTagService = () => {
         (window as any).googletag = (window as any).googletag || { cmd: [] };
@@ -57,10 +42,17 @@ const SDK = (props: { onCompleted: () => void }) => {
         googletag.cmd.push(function () {
             googletag.pubads().setForceSafeFrame(true);
 
-            SLOT_LIST.forEach(function (slotItem, slotItemIndex) {
-                googletag
-                    .defineSlot(slotItem.unit, slotItem.size, slotItem.id)
-                    .addService(googletag.pubads());
+            SLOT_FIXED_LIST.forEach((slot) => {
+                const containers = document.getElementsByClassName(slot.class);
+                Array.from(containers).forEach((container, containerIndex) => {
+                    const uniqueId = slot.class + '-' + containerIndex;
+                    container.id = uniqueId;
+
+                    googletag
+                        .defineSlot(slot.unit, slot.size, uniqueId)
+                        .setForceSafeFrame(true)
+                        .addService(googletag.pubads());
+                });
             });
 
             googletag.pubads().addEventListener('slotRenderEnded', (event: any) => {
@@ -68,21 +60,44 @@ const SDK = (props: { onCompleted: () => void }) => {
                 const slotContainer = document.getElementById(slotId);
                 const safeframe = slotContainer?.querySelector(
                     'iframe[id^="google_ads_iframe"]'
-                );
+                ) as HTMLIFrameElement;
 
-                if (safeframe && slotContainer) {
-                    slotContainer.children[0].style.width = '100%';
-                    safeframe.width = '100%';
-                }
+                safeframe.onload = () => {
+                    safeframe?.contentWindow?.postMessage(
+                        {
+                            type: 'pinkoi-gam-config',
+                            selector: 'ins[data-pinkoi-gam-client="data-pinkoi-gam"]',
+                            viewId,
+                            containerId: slotId,
+                            href: window.location.href,
+                            mb: '20250114r9HXY6C5Zh',
+                        },
+                        '*'
+                    );
+                };
+
+                window.addEventListener('message', function (event) {
+                    if (
+                        event.data.type === 'pinkoi-gam-layout-change' &&
+                        event.data.container_id === slotId
+                    ) {
+                        safeframe.style.display = 'block';
+                        safeframe.style.width = '100%';
+                        safeframe.style.height = `${event.data.dimension.height}px`;
+                        safeframe.height = `${event.data.dimension.height}px`;
+
+                        slotContainer.children[0].style.width = '100%';
+                        slotContainer.children[0].style.height = `${event.data.dimension.height}px`;
+                    }
+                });
 
                 event.slot.setSafeFrameConfig(safeFrameConfig);
             });
 
             googletag.pubads().enableSingleRequest();
             googletag.enableServices();
+            googletag.pubads().refresh();
         });
-
-        onCompleted();
     };
 
     return (
@@ -98,3 +113,11 @@ const SDK = (props: { onCompleted: () => void }) => {
 };
 
 export default SDK;
+
+
+{/* <h4 style="font-size:18px; font-weight: bold; text-align:left; margin-bottom:20px;">探索更多在 Pinkoi 平台上相關的商品</h4>
+<ins
+ data-pinkoi-zine-promoted-client="data-pinkoi-zine-promoted"
+ data-pinkoi-zine-promoted-type="listing_rectangle"
+ data-pinkoi-zine-promoted-section="post_bottom"
+/> */}
