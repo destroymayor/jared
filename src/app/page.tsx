b0eb27d339e3b34459d3877e98bb50f9ebb41672
Jared Chen <kuil5230@gmail.com>
2025-04-13 21:06:59 +0800

    feat: add work block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from 'next/link';
 import Glow from '@/components/ui/glow';
+import Work from './work';
 import Footer from './footer';
 
 export default function Page() {
@@ -10,31 +10,17 @@ export default function Page() {
             <div className='absolute inset-0 overflow-hidden pointer-events-none'>
                 <Glow />
             </div>
-            <div className="flex min-h-[400px] flex-col gap-3 pt-12">
+            <div className="flex pb-12 sm:pb-44 flex-col gap-3 pt-32 sm:pt-12">
                 <h1 className="text-4xl font-extrabold">Jared</h1>
                 <h2 className="text-zinc-800 dark:text-zinc-300">Front-End Developer</h2>
-
                 <hr className="w-28 border border-zinc-300 dark:border-zinc-700" />
-
                 <p className="pt-2 text-sm leading-7 text-zinc-800 dark:text-zinc-300">
                     I&#39;m <b>Jared Chen</b>, a Front-end Developer working with{' '}
                     <b>React</b> and <b>Next.js</b> who enjoys building great quality and
                     great user experience products.
                 </p>
-
-                <p className="pt-4 text-sm leading-7 text-zinc-800 dark:text-zinc-300">
-                    <span>Currently working at </span>
-                    <Link
-                        href="https://www.pinkoi.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="inline-flex items-end gap-2"
-                    >
-                        <span className="text-sky-600 hover:underline">Pinkoi</span>
-                    </Link>
-                    .{' '}
-                </p>
             </div>
+            <Work />
             <Footer />
         </>
     );
---
+++ src/app/work.tsx
@@ -0,0 +1,67 @@
+import pinkoi from '@/public/images/work/pinkoi.webp';
+import tagtoo from '@/public/images/work/tagtoo.webp';
+
+import { BriefcaseBusiness} from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+const workHistory = [
+    {
+        logo: (
+            <Avatar className="h-10 w-10">
+                <AvatarImage src={pinkoi.src} alt="Pinkoi" />
+                <AvatarFallback>P</AvatarFallback>
+            </Avatar>
+        ),
+        company: 'Pinkoi',
+        position: 'Frontend Engineer',
+        duration: '08 / 2022 - Present',
+    },
+    {
+        logo: (
+            <Avatar className="h-10 w-10">
+                <AvatarImage src={tagtoo.src} alt="Tagtoo" />
+                <AvatarFallback>T</AvatarFallback>
+            </Avatar>
+        ),
+        company: 'Tagtoo',
+        position: 'Frontend Engineer',
+        duration: '09 / 2019 - 08 / 2022',
+    },
+];
+
+const Work = () => {
+    return (
+        <div className="flex flex-col gap-4 rounded-2xl border border-zinc-200 p-6 dark:border-zinc-700">
+            <h2 className="flex items-center gap-2">
+                <BriefcaseBusiness
+                    width={20}
+                    height={20}
+                    className="text-zinc-900 dark:text-zinc-400"
+                />
+                <span>Work</span>
+            </h2>
+            <ol className="flex flex-col gap-4">
+                {workHistory.map((work) => (
+                    <li key={work.company} className="flex items-center gap-4">
+                        <div className="rounded-full border border-zinc-200 p-2 dark:border-zinc-700 dark:bg-zinc-900">
+                            {work.logo}
+                        </div>
+                        <div className="flex-1">
+                            <h3>{work.company}</h3>
+                            <div className="flex items-center justify-between">
+                                <p className="text-sm text-zinc-500 dark:text-zinc-300">
+                                    {work.position}
+                                </p>
+                                <p className="text-sm text-zinc-500 dark:text-zinc-400">
+                                    {work.duration}
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
+};
+
+export default Work;
