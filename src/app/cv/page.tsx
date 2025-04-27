7aabd3a5c5246a182ee6c36c66e84b042be4d8c2
Jared Chen <kuil5230@gmail.com>
2025-04-27 13:59:15 +0800

    chore: remove unused component

--- src/app/cv/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Page() {
-    return (
-        <div >
-            <h1>WIP :)</h1>
-        </div>
-    );
-}
--- src/components/Tabs/index.tsx
+++
@@ -1,8 +0,0 @@
-import Tabs from './Tabs';
-import Tab from './Tab';
-
-const pkg = Object.assign(Tabs, {
-    Tab,
-});
-
-export default pkg;
